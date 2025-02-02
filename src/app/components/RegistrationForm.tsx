2d48239a4c84d4a08278a275527a5f9446990857
Kanchi Kovil <kanchikovil2024@gmail.com>
2025-02-02 17:21:15 +0530

    Material UI

--- src/app/components/RegistrationForm.tsx
+++ src/app/components/RegistrationForm.tsx
@@ -2,7 +2,7 @@
 // src/components/RegistrationForm.tsx
 import React, { useState, useEffect } from 'react';
 import { createRegistration, editRegistration } from '../services/registrationService';
-import { Box, TextField, Typography,Button } from '@mui/material';
+import { Box, TextField, Typography, Button } from '@mui/material';
 
 interface RegistrationFormProps {
   initialData?: { user_name: string, user_phone: string, id?: number };
@@ -46,42 +46,171 @@ const RegistrationForm: React.FC<RegistrationFormProps> = ({ initialData, onSucc
 
       {/* <form onSubmit={handleSubmit}> */}
       <div>
+      <TextField
+          id="outlined-disabled"
+          label="Aadhar Number"
+          value={phoneNumber}
+          placeholder='Aadhar number here...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Contact Number"
+          value={phoneNumber}
+          placeholder='Phone number here...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
         <TextField
           required
           id="outlined-required"
-          label="Registration Name"
+          label="Name of the Girl"
           placeholder='Your Name here...'
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
-        <TextField
-          
-          id="outlined-disabled"
-          label="Phone Number"
-          value={phoneNumber}
-          placeholder='phoneNumber here...'
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-      </div>
-      {/* <h1>Registration Form</h1>
-      <div>
-        <label>Registration Name</label>
-        <input
-          type="text"
+                <TextField
+          required
+          id="outlined-required"
+          label="Horoscope Name"
+          placeholder='Horoscope Name...'
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
-          required
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Age"
+          value={phoneNumber}
+          placeholder='Your Age here...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Name of School"
+          value={phoneNumber}
+          placeholder='Your SChool here...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Std Studying"
+          value={phoneNumber}
+          placeholder='Your Std...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Stotram / Slogan"
+          value={phoneNumber}
+          placeholder='Stotram / Slogan known...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Classical Music Known"
+          value={phoneNumber}
+          placeholder='Vocal / Instruments...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Mother Toungue"
+          value={phoneNumber}
+          placeholder='Mother Toungue..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Native"
+          value={phoneNumber}
+          placeholder='Native Of..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Father's Name"
+          value={phoneNumber}
+          placeholder='Fathers Name..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Father's Gothram"
+          value={phoneNumber}
+          placeholder='Fathers Gothram..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Father's Vedam"
+          value={phoneNumber}
+          placeholder='Fathers Vedam..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Father's Profession"
+          value={phoneNumber}
+          placeholder='Fathers Profession..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Mother's Name"
+          value={phoneNumber}
+          placeholder='Mothers Name..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Maternal GRandfathers Gothram"
+          value={phoneNumber}
+          placeholder='Grand Fathers Gothram..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Mother's Vedam"
+          value={phoneNumber}
+          placeholder='Mothers Vedam..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Mother's Profession"
+          value={phoneNumber}
+          placeholder='Mothers Profession..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Kula Devatha"
+          value={phoneNumber}
+          placeholder='Kula Devatha..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Place"
+          value={phoneNumber}
+          placeholder='Place..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Residential Address"
+          value={phoneNumber}
+          placeholder='Address..'
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <TextField
+          id="outlined-disabled"
+          label="Referred By"
+          value={phoneNumber}
+          placeholder='Referred By...'
+          onChange={(e) => setPhoneNumber(e.target.value)}
         />
       </div>
-      <div>
-        <label>Phone Number</label>
-        <input
-          type="text"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          required
-        />
-      </div> */}
+
       <Button variant="contained" type="submit">Register</Button>
       {/* <button type="submit">Submit</button> */}
       {/* </form> */}
