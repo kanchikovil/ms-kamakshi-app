3efbb98de666595022e79853466622c9b18e85fa
Prithvi <k.t.prithvi@gmail.com>
2025-03-08 19:45:09 +0530

    registration logic
    registration logic

--- src/app/pages/navratri-registration/page.tsx
+++ src/app/pages/navratri-registration/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 // src/components/RegistrationForm.tsx
 import React, { useState, useEffect } from 'react';
-import { createRegistration, editRegistration } from '@/app/services/registrationService';
-import { Box, TextField, Typography, Button } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import KanyaRegistrationForm1 from '@/app/components/KanyaRegistrationForm1';
-import SuvashiniRegistrationForm from '../suvashini-registration/page';
 import { useNotification } from '@/app/context/NotificationContext';
 import CheckStatusCard from '@/app/components/CheckStatusCard';
+import NavratriRegistrationForm from '@/app/components/NavratriRegistrationForm';
 
 interface RegistrationFormProps {
  // initialData?: { userName: string, userPhone: string, id?: number };
@@ -18,8 +13,6 @@ interface RegistrationFormProps {
 }
 
 const RegistrationForm: React.FC<RegistrationFormProps> = ({ /*initialData , onSuccess*/ }) => {
-  const [userType, setUserType] = useState(true);
-  const [typeLabel, setTypeLabel] = useState('Kanya');
 
   const { showSuccess, showError } = useNotification();
 
@@ -35,13 +28,7 @@ const RegistrationForm: React.FC<RegistrationFormProps> = ({ /*initialData , onS
     </Grid>
     <Grid size={{ xs: 7 }}>
       <Box sx={{ height: '100%', fontSize: 30, borderRadius: 1, margin: 1, backgroundColor:'#FFFFFF' }}>
-        <KanyaRegistrationForm1 
-              onSuccess={function (): void {
-                showSuccess('Registration Created Successfully')
-              } } 
-              onError={ function (): void {
-                showError('Registration Failed')
-              }  } />
+        <NavratriRegistrationForm />
       </Box>
     </Grid>
     <Grid size={{ xs: 3 }}>
