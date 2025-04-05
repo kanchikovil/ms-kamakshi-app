b6b301ce8b20b0ce4ae133960b82c5f7f61b40c3
Kanchi Kovil <kanchikovil2024@gmail.com>
2025-04-05 20:59:05 +0530

    Added Logging to Debug

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -3,5 +3,5 @@ const APP_CONFIG = {
     apiBaseUrl: process.env.API_BASE_URL || "http://localhost:8080/api",
     recaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LfVmewqAAAAAIPZGb3ASDDjjrG-meg07kZn5HwF"
 };
-
+console.log("APP_CONFIG", JSON.stringify(APP_CONFIG));
 export default APP_CONFIG;
