c13496d5bd34db17375274ef455dccae3e99c8dc
Kanchi Kovil <kanchikovil2024@gmail.com>
2025-04-06 09:10:02 +0530

    Hard coded api url to check

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
     env: process.env.APP_ENV,
-    apiBaseUrl: process.env.API_BASE_URL || "http://localhost:8080/api",
+    apiBaseUrl: process.env.API_BASE_URL || "https://kanchikovil-be-api-g2f4fbb0hbdkhqd9.canadacentral-01.azurewebsites.net/api",
     recaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LfVmewqAAAAAIPZGb3ASDDjjrG-meg07kZn5HwF"
 };
 console.log("APP_CONFIG", JSON.stringify(APP_CONFIG));
