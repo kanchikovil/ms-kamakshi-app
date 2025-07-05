b42c58d6a1b680a125b0df0d14b4851afc6fc303
KanchiKovil <kanchikovil2024@gmail.com>
2025-07-05 17:23:24 +0530

    Updated Remote URL

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
     env: process.env.APP_ENV,
-    apiBaseUrl: process.env.API_BASE_URL || "http://localhost:3000/api",
+    apiBaseUrl: process.env.API_BASE_URL || "https://kanchikovil-be-api-g2f4fbb0hbdkhqd9.canadacentral-01.azurewebsites.net/api",
     // apiBaseUrl: "http://localhost:8080/api", 
     recaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LeHkA0rAAAAABAyc--8zcRtb9NlWIQjPDTxpY11"
 };
